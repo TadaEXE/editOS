3f57dcd323b7e5cf2d20612c216d936fc6f59822
TadaEXE <36825573+TadaEXE@users.noreply.github.com>
2025-12-02 05:23:29 +0100

    Added source location info to panic

--- src/kernel/panic.hpp
+++ src/kernel/panic.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
+#include <source_location>
 #include <utility>
 
 #include "kernel/log.hpp"
 
 template <typename... Args>
-[[noreturn]] void panic(const char* fmt, Args&&... args) {
+[[noreturn]] void panic(
+    const char* fmt, Args&&... args,
+    const std::source_location location = std::source_location::current()) {
   log_msg("[KERNEL PANIC] \\");
   log_msg(fmt, std::forward<Args>(args)...);
+  log_msg("at %s:%d in `%s`", location.file_name(), location.line(),
+          location.function_name());
   for (;;) {}
 }
